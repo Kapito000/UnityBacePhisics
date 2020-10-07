ab651b6cd3802c6a98e4582a05438be1e86597c0
Kapito000 <san.9ul@yandex.ru>
2020-10-07 21:28:20 +0300

    AddForce

---
+++ Assets/Scripts/IgnoreCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollision : MonoBehaviour
+{
+    [SerializeField] Collider _one;
+    [SerializeField] Collider _two;
+
+    private void Awake()
+    {
+        if (!_one) _one = GetComponent<Collider>();
+    }
+
+    private void Start()
+    {
+        Physics.IgnoreCollision(_one, _two, true);
+    }    
+}
