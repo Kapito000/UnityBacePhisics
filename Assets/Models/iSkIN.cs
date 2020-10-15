bb7707d6e5a8937eb44dfe5ca0bcf0c74fa2e042
Kapito000 <san.9ul@yandex.ru>
2020-10-15 18:03:16 +0300

    Phosics end

---
+++ Assets/Models/iSkIN.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iSkIN : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    [SerializeField] Rigidbody[] _rigidbody;
+
+    private void Start()
+    {
+        foreach (var rb in _rigidbody)
+        {
+            rb.isKinematic = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKey(KeyCode.O)) return;
+
+        _animator.enabled = false;
+        foreach (var rb in _rigidbody)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
